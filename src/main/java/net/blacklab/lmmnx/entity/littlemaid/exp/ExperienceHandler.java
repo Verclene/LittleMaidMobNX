a3985fdc688e9f378b0ad748e6b50c5a255e27a7
Verclene <xdev58bo@gmail.com>
2016-02-22 19:10:25 +0900

    5.0.55: Add experience reward "Max HP Boost"

--- src/main/java/net/blacklab/lmmnx/entity/littlemaid/exp/ExperienceHandler.java
+++ src/main/java/net/blacklab/lmmnx/entity/littlemaid/exp/ExperienceHandler.java
@@ -1,17 +1,58 @@
 package net.blacklab.lmmnx.entity.littlemaid.exp;
 
+import java.util.UUID;
+
 import littleMaidMobX.LMM_EntityLittleMaid;
+import net.minecraft.entity.SharedMonsterAttributes;
+import net.minecraft.entity.ai.attributes.AttributeModifier;
+import net.minecraft.entity.ai.attributes.IAttributeInstance;
+import net.minecraft.util.MathHelper;
 
 public class ExperienceHandler {
 	
 	protected LMM_EntityLittleMaid theMaid;
+
+	private static final String uuidString = "NX_EXP_HP_BOOSTER";
+	public static final UUID NX_EXP_HP_BOOSTER = UUID.nameUUIDFromBytes(uuidString.getBytes());
 
 	public ExperienceHandler(LMM_EntityLittleMaid maid) {
 		theMaid = maid;
 	}
 	
 	public void onLevelUp(int level) {
-		
+		/*
+		 * 最大HP上昇
+		 */
+		double modifyamount = 0;
+		double prevamount = 0;
+		if (level > 30) {
+			modifyamount += (Math.min(level, 50)-30)/2;
+		}
+		if (level > 50) {
+			modifyamount += MathHelper.floor_float((Math.min(level, 75)-50)/2.5f);
+		}
+		if (level > 75) {
+			modifyamount += MathHelper.floor_float((Math.min(level, 150)-75)/7.5f);
+		}
+		if (level > 150) {
+			modifyamount += (Math.min(level, 300)-150)/15;
+		}
+		IAttributeInstance maxHPattr = theMaid.getEntityAttribute(SharedMonsterAttributes.maxHealth);
+		AttributeModifier existedMod = maxHPattr.getModifier(NX_EXP_HP_BOOSTER);
+		if (existedMod != null) {
+			prevamount = existedMod.getAmount();
+		}
+		if (modifyamount != 0 || prevamount < modifyamount) {
+			// Modifier書き換え
+			float prevHP = theMaid.getHealth();
+			AttributeModifier maxHPmod = new AttributeModifier(NX_EXP_HP_BOOSTER, uuidString, modifyamount, 0);
+			if (existedMod != null) {
+				maxHPattr.removeModifier(existedMod);
+			}
+			maxHPattr.applyModifier(maxHPmod);
+			// たぶんremoveModifierした時に20を超える体力が削られちゃうので，再設定．
+			theMaid.setHealth(prevHP);
+		}
 	}
 	
 	public void onUpdate() {
